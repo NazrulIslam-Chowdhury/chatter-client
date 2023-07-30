f299d265560f8f63b8e5cefbbfcce9eea5ea2c31
unknown <nazrulislam66188@gmail.com>
2023-07-30 09:59:17 +0600

    team channel list

--- src/components/ChannelListContainer.js
+++ src/components/ChannelListContainer.js
@@ -1,26 +1,54 @@
 import React from 'react';
 import { ChannelList, useChatContext } from 'stream-chat-react';
-import { ChannelSearch, SideBar, TeamChannelList, TeamChannelPreview } from './';
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 
 import hospitalIcon from '../assets/hospital.png';
 import logoutIcon from '../assets/logout.png';
 
 import Cookies from 'universal-cookie';
 
+const SideBar = () => {
+    return (
+        <div className='channel-list__sidebar'>
+            <div className='channel-list__sidebar__icon1'>
+                <div className='icon1__inner'>
+                    <img src={hospitalIcon} alt="Hospital" width={30} />
+                </div>
+            </div>
+            <div className='channel-list__sidebar__icon2'>
+                <div className='icon1__inner'>
+                    <img src={logoutIcon} alt="Hospital" width={30} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const CompanyHeader = () => {
+    return (
+        <div className='channel-list__header'>
+            <p className='channel-list__header__text'>Medical Chatter</p>
+        </div>
+    )
+}
+
 const ChannelListContainer = () => {
     return (
         <>
-            <div className='channel-list__sidebar'>
-                <div className='channel-list__sidebar__icon1'>
-                    <div className='icon1__inner'>
-                        <img src={hospitalIcon} alt="Hospital" width={30} />
-                    </div>
-                </div>
-                <div className='channel-list__sidebar__icon2'>
-                    <div className='icon1__inner'>
-                        <img src={logoutIcon} alt="Hospital" width={30} />
-                    </div>
-                </div>
+            <SideBar />
+            <div className='channel-list__list__wrapper'>
+                <CompanyHeader />
+                <ChannelSearch />
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => { }}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            {...listProps}
+                            type='team'
+                        />
+                    )}
+                />
             </div>
         </>
     )
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,12 @@
 import ChannelContainer from "./ChannelContainer";
 import ChannelListContainer from "./ChannelListContainer";
+import ChannelSearch from "./ChannelSearch";
+import TeamChannelList from "./TeamChannelList";
 
 
 export {
     ChannelContainer,
     ChannelListContainer,
+    ChannelSearch,
+    TeamChannelList
 }
