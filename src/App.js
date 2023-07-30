3b38a0b590846601bef9f4248de415ccb7348ef2
unknown <nazrulislam66188@gmail.com>
2023-07-30 11:18:06 +0600

    auth section created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 
 import Cookies from 'universal-cookie'
-import { ChannelContainer, ChannelListContainer } from './components';
+import { ChannelContainer, ChannelListContainer, Auth } from './components';
 
 const apiKey = 'bf9ep5fdutmf';
 const client = StreamChat.getInstance(apiKey)
 
+const authToken = false;
+
 function App() {
+  if (!authToken) return <Auth />
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme='team light'>
--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,15 @@ import ChannelContainer from "./ChannelContainer";
 import ChannelListContainer from "./ChannelListContainer";
 import ChannelSearch from "./ChannelSearch";
 import TeamChannelList from "./TeamChannelList";
+import TeamChannelPreview from "./TeamChannelPreview";
+import Auth from "./Auth";
 
 
 export {
     ChannelContainer,
     ChannelListContainer,
     ChannelSearch,
-    TeamChannelList
+    TeamChannelList,
+    TeamChannelPreview,
+    Auth
 }
